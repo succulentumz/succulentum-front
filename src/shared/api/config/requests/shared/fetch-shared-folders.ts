75f9c41ef1c552e230fea1f92262d3d50c23ff4c
v.lagerev <v.lagerev@g.nsu.ru>
2025-12-06 20:14:48 +0700

    Request fixes

--- src/shared/api/config/requests/shared/fetch-shared-folders.ts
+++ src/shared/api/config/requests/shared/fetch-shared-folders.ts
@@ -1,5 +1,10 @@
-import { type ICollection, type IFolderRaw, type IPageable, pageableMax } from '@/shared/api';
 import { foldersRaw } from '@/shared/api/config/fixtures/folders';
+import {
+  type ICollection,
+  type IFolderRaw,
+  type IPageable,
+  pageableMax,
+} from '@/shared/api/config/model';
 
 import { hosts } from '../../../../config';
 import { createFetchConfig } from '../../../helpers';
